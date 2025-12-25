10d4181ba0609181cde935b34d44d3bd32c2317a
devurmil <urmilramani124@gmail.com>
2025-12-25 16:22:45 +0530

    feat: Add `AnimatePresence` for modal exit animation and implement outside click to close.

--- src/components/Konami/Konami.jsx
+++ src/components/Konami/Konami.jsx
@@ -1,18 +1,27 @@
 import useKonami from "../../hooks/useKonami";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRef } from "react";
 
 function Konami() {
   const { activated, setActivated } = useKonami();
+  const modalRef = useRef(null);
 
   return (
-    <>
+    <AnimatePresence>
       {activated && (
         <motion.div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.4 }}
+        onClick={(event) => {
+          if (modalRef.current && !modalRef.current.contains(event.target)) {
+            setActivated(false);
+          }
+        }}
         >
-          <div className="bg-white/30 backdrop-blur-sm p-6 rounded-xl text-center">
+          <div className="bg-white/30 backdrop-blur-sm p-6 rounded-xl text-center"
+          ref={modalRef}>
             <h2 className="text-2xl font-semibold mb-3">🎉 You found the secret!</h2>
             <button 
             onClick={() => setActivated(false)}
@@ -21,7 +30,7 @@ function Konami() {
           </div>
         </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 }
 
